c747feb37fc0e7ca6ba1f4f2c7336f21f9390564
Shivam-Omen <srvcode@gmail.com>
2025-06-23 08:38:46 +0530

    Adding initial commit for Spring boot mongo db

---
+++ springboot-data-mongo/src/main/java/com/geek/mongo/SpringbootDataMongoApplication.java
@@ -0,0 +1,13 @@
+package com.geek.mongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootDataMongoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootDataMongoApplication.class, args);
+	}
+
+}
